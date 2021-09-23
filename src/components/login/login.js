c5e9ff8d316f4834c7b6dd6dab2680ebd9708aba
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-09-24 00:23:22 +0100

    Implemented Login and made a fix on Register

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 
 import './login.scss';
 
@@ -19,10 +19,17 @@ class Login extends Component {
 
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({email: '', password: ''})
+    const { email, password } = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: ''})
+    } catch (error) {
+      console.log(error)
+    }
 
   }
 
