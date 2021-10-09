aed8666d23a04ea3dcba66d929c82acb04362fec
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-10-09 23:20:12 +0100

    Adding cart and implementing redux on cart

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button";
 
 import './collection-item.scss';
 
@@ -14,7 +15,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-        
+        <CustomButton inverted>Add to Cart</CustomButton>
     </div>
 
 )
